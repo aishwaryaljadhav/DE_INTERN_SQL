b2b53271043f55519791e6b6a7cfb2e73179e0cc
aishwaryaljadhav <aishwaryaljadhav18@gmail.com>
2026-02-22 19:01:41 +0530

    aggregate functions

---
+++ src/Aggregate_func/aggregate_func.sql
@@ -0,0 +1,16 @@
+select max(marks)
+from student;
+
+select *from student;
+
+select count(name) from student;
+
+select min(marks)
+from student;
+
+select sum(marks)
+from student;
+
+select avg(marks)
+from student;
+
