5cafea91230425404551380b017cdcdcf6a5fdf1
aishwaryaljadhav <aishwaryaljadhav18@gmail.com>
2026-02-22 18:59:14 +0530

    operators

--- src/Operators/operators_with_where_clause.sql
+++ src/Operators/operators_with_where_clause.sql
@@ -1,5 +1,22 @@
+
+USE COLLEGE;
 select *from student where marks+10>100 ;
 
 select *from student where marks=93 ;
 
 select *from student where marks>90 ;
+
+select *from student where marks>80 and city="Mumbai";
+
+select *from student where marks>90 or city="Mumbai";
+
+select *from student where marks between 80 and 90; 
+
+select *from student where city in ("Delhi", "Mumbai");
+
+select *from student where city not in ("Delhi", "Mumbai");
+
+
+
+ 
+
