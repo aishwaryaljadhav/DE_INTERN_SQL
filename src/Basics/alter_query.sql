5e2e7c22e2eef8509ddd3bf9806fe59e4289b59f
aishwaryaljadhav <aishwaryaljadhav18@gmail.com>
2026-02-23 15:38:44 +0530

    truncate and alter query

---
+++ src/Basics/alter_query.sql
@@ -0,0 +1,24 @@
+alter table student
+add column age int not null default 19;
+
+select *from student;
+
+alter table student
+modify column age varchar(2);
+
+alter table student
+change age stu_age int;
+
+alter table student
+drop column stu_age;
+
+alter table student 
+rename to stu;
+
+alter table stu
+rename to student;
+
+
+
+
+
